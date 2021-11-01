47232d63f7fd607a54892947d8a3063b01033926
Tim <tim_becker@online.de>
2021-11-01 19:11:57 +0100

    first running version

--- midterm_project/README.md
+++ midterm_project/README.md
@@ -1,2 +1,18 @@
 # zoomcap
 midterm project for ML zoomcamp course: https://github.com/alexeygrigorev/mlbookcamp-code/tree/master/course-zoomcamp
+
+# link to dataset
+Medical cost personal dataset from kaggle:
+https://www.kaggle.com/mirichoi0218/insurance
+
+# description of dataset (columns)
+- age: age of primary beneficiary
+- sex: insurance contractor gender, female, male
+- bmi: Body mass index, providing an understanding of body, weights that are relatively high or low relative to height, objective index of body weight (kg / m ^ 2) using the ratio of height to weight, ideally 18.5 to 24.9
+- children: Number of children covered by health insurance / Number of dependents
+- smoker: Smoking
+- region: the beneficiary's residential area in the US, northeast, southeast, southwest, northwest.
+- charges: Individual medical costs billed by health insurance
+
+# goal of the midterm project
+The task of the project is to create a model that can predict the personal insurance costs (column: charges) by using the provided features.
--- midterm_project/train.py
+++ midterm_project/train.py
@@ -1,1 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
 import pandas as pd
+import numpy as np
+import pickle
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeRegressor
+
+
+def get_pipeline(model):
+    categorical_features = ['smoker', 'sex', 'region']
+
+    categorical_transformer = OneHotEncoder(handle_unknown='ignore')
+    preprocessor = ColumnTransformer(
+        transformers=[('cat', categorical_transformer, categorical_features)])
+
+    pipe = Pipeline(
+        steps=[('preprocessor', preprocessor), ('model', model())]
+    )
+    return pipe
+
+
+def train(X_train, y_train):
+    tree_pipe = get_pipeline(DecisionTreeRegressor)
+
+    search = GridSearchCV(tree_pipe, params_tree, cv=num_cv, refit=True, verbose=0, n_jobs=1,
+                          scoring='neg_mean_squared_error')
+    search.fit(X_train, y_train)
+
+    with open(model_file_name, 'wb') as f_out:
+        pickle.dump(search, f_out)
+
+    print(f'the model is saved to {model_file_name}')
+
+
+if __name__ == '__main__':
+    # parameters
+    num_cv = 4
+    model_file_name = os.environ.get('MODEL_FILE_NAME', 'best_model.bin')
+    params_tree = [
+        {
+            'model__max_depth': [4, 5, 6, 8],
+            'model__min_samples_leaf': [10, 20, 25]
+        }
+    ]
+    # data preparation
+    data = pd.read_csv('insurance.csv')
+    df_train, df_test = train_test_split(data, test_size=0.2, random_state=1, stratify=data['smoker'])
+    df_train['charges'] = np.log1p(df_train['charges'])
+    df_test['charges'] = np.log1p(df_test['charges'])
+    y_train = df_train.pop('charges')
+    y_test = df_test.pop('charges')
+
+    train(df_train, y_train)
+
+
+
